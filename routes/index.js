9bae0373fc5e04b700243a6a0ebb1de8f77844be
项罗阳 <chaosx@yeah.net>
2017-06-28 18:12:09 +0800

    upload

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+/*Get Hello World Page*/
+router.get('/helloworld',function(req,res){
+  res.render('helloworld',{title:'hello,world'});
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
