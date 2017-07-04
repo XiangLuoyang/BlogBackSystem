9698373e9d60fb310239c2e19d3eb5b1c82fc426
项罗阳 <chaosx@yeah.net>
2017-07-04 13:24:14 +0800

    封装接口

--- app.js
+++ app.js
@@ -7,8 +7,35 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+//引入数据库连接文件
 var conn = require('./data/connect');
+
 var app = express();
+//设置跨域访问
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1')
+    if (req.method == "OPTIONS") res.send(200); /*让options请求快速返回*/
+    else next();
+});
+
+//引入API 1 question的json
+var questions=require('./data/questions');
+//写个接口123
+app.get('/123',function(req,res){
+res.status(200),
+res.json(questions)
+});
+
+
+//配置服务端口
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,7 +45,9 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,14 +55,14 @@ app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ data/questions.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var questions = [{
+        data: 213,
+        num: 444,
+        age: 12
+    },
+    {
+        data: 456,
+        num: 678,
+        age: 13
+    },
+    {
+        data: "test",
+        num: "king",
+        age: "666"
+    }
+];
+
+module.exports=questions;
