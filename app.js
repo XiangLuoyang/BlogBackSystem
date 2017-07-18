cbbd9a6a7c3fde0ef3a44080fb6e0cbab4a27327
项罗阳 <chaosx@yeah.net>
2017-07-18 12:51:12 +0800

    change port to 8080

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ res.json(userInfo)
 
 
 //配置服务端口
-var server = app.listen(3000, function () {
+var server = app.listen(8080, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
